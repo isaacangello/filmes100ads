62d1f278320a4a3abea06d1480af603a1f9f1e02
isaacangello <isaacangello@gmail.com>
2019-02-19 17:31:54 -0300

    Adicionando CoinHiveApi

--- resources/views/adm/adm.blade.php
+++ resources/views/adm/adm.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-xl-3 col-sm-6 mb-3">
-                <div class="card text-white bg-primary o-hidden h-100">
+                <div class="card text-white bg-info o-hidden h-100">
                     <div class="card-body">
                         <div class="card-body-icon">
                             <i class="fas fa-fw fa-user-circle"></i>
@@ -22,12 +22,28 @@
                 </div>
             </div>
             <div class="col-xl-3 col-sm-6 mb-3">
-                <div class="card text-white bg-warning o-hidden h-100">
+                <div class="card text-white bg-info o-hidden h-100">
                     <div class="card-body">
                         <div class="card-body-icon">
-                            <i class="fas fa-fw fa-list"></i>
+                            <i class="fas fa-fw fa-save"></i>
                         </div>
                         <div class="mr-5">Cadastrar Filme(s)</div>
+                    </div>
+                    <a class="card-footer text-white clearfix small z-1" href="#">
+                        <span class="float-left">Ver Detalhes</span>
+                        <span class="float-right">
+                        <i class="fas fa-angle-right"></i>
+                      </span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-white bg-info o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fas fa-fw fa-list-alt"></i>
+                        </div>
+                        <div class="mr-5">Meus Filmes Cadastrados</div>
                     </div>
                     <a class="card-footer text-white clearfix small z-1" href="#">
                         <span class="float-left">Ver Detalhes</span>
@@ -41,23 +57,7 @@
                 <div class="card text-white bg-success o-hidden h-100">
                     <div class="card-body">
                         <div class="card-body-icon">
-                            <i class="fas fa-fw fa-shopping-cart"></i>
-                        </div>
-                        <div class="mr-5">Meus Filmes Cadastrados</div>
-                    </div>
-                    <a class="card-footer text-white clearfix small z-1" href="#">
-                        <span class="float-left">Ver Detalhes</span>
-                        <span class="float-right">
-                        <i class="fas fa-angle-right"></i>
-                      </span>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xl-3 col-sm-6 mb-3">
-                <div class="card text-white bg-danger o-hidden h-100">
-                    <div class="card-body">
-                        <div class="card-body-icon">
-                            <i class="fas fa-fw fa-life-ring"></i>
+                            <i class="fas fa-fw fa-comment-dollar"></i>
                         </div>
                         <div class="mr-5">Rendimentos</div>
                     </div>
@@ -69,7 +69,26 @@
                     </a>
                 </div>
             </div>
+
+            <div class="col-xl-3 col-sm-6 mb-3">
+                <div class="card text-info bg-light o-hidden h-100">
+                    <div class="card-body">
+                        <div class="card-body-icon">
+                            <i class="fas fa-fw fa-comment-dots"></i>
+                        </div>
+                        <div class="mr-5">Ajuda</div>
+                    </div>
+                    <a class="card-footer text-info clearfix small z-1" href="#">
+                        <span class="float-left">Ver Detalhes</span>
+                        <span class="float-right">
+                        <i class="fas fa-angle-right"></i>
+                      </span>
+                    </a>
+                </div>
+            </div>
         </div>
+
     </div>
+
 
 @endsection
