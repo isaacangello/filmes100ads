aa5c72ed79e4e1f1d521e276751e6c9409e40652
isaacangello <isaacangello@gmail.com>
2019-02-19 22:17:44 -0300

    ADicinandi novo controller

---
+++ app/Http/Controllers/RendimentosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RendimentosController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+            return view(
+                'adm.rendimentos'
+            );
+    }
+
+}
